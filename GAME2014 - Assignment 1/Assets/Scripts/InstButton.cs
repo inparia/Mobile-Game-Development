afa6a5635975a60309cccffcc63879360dbffe62
inparia <inparia@live.ca>
2020-10-02 18:29:28 -0400

    Putting files in correct folders

---
+++ GAME2014 - Assignment 1/Assets/Scripts/InstButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstButton : MonoBehaviour
+{
+    public Button button;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.gameObject.SetActive(false);
+        button.onClick.AddListener(SetButton);
+    }
+
+    private void Update()
+    {
+    }
+    // Update is called once per frame
+
+    public void SetButton()
+
+    {
+        text.gameObject.SetActive(true);
+        button.gameObject.SetActive(false);
+    }
+}
