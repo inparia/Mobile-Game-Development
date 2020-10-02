47acb44a402d9dd08b4b5c1cb4034228fbc06ab7
inparia <inparia@live.ca>
2020-10-02 18:06:48 -0400

    Assignment 1
    up to 95% done,
    Hazard, Player, UIs, Done right now
    Sound, and comments left to do

---
+++ GAME2014 - Assignment 1/Assets/RockCollision.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RockCollision : MonoBehaviour
+{
+    //Creating variables
+    public GameObject player, lifeOne, lifeTwo, lifeThree;
+    public Button button;
+    public int life;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initializing Life to 3, and display lives on screen
+        life = 3;
+        lifeOne.gameObject.SetActive(true);
+        lifeTwo.gameObject.SetActive(true);
+        lifeThree.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Rock"))
+        {
+
+            //Disable the rock
+            col.gameObject.SetActive(false);
+
+            //Decrement Life by 1 everytime player hits the rock
+            life--;
+
+            //Disable player's life sprite each time player loses life.
+            if(life == 2)
+            {
+                lifeThree.gameObject.SetActive(false);
+            }
+            if (life == 1)
+            {
+                lifeTwo.gameObject.SetActive(false);
+            }
+            //If life reaches 0, game over.
+            if (life == 0)
+            {
+                lifeOne.gameObject.SetActive(false);
+                player.gameObject.SetActive(false);
+                button.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ GAME2014 - Assignment 1/Assets/StartButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+
+    public Button button;
+    public Button buttonTwo;
+    public Button buttonThree;
+    public GameObject player;
+    public Text instText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(SetButton);
+    }
+
+    private void Update()
+    {
+    }
+    // Update is called once per frame
+
+    public void SetButton()
+
+    {
+        player.gameObject.SetActive(true);
+        button.gameObject.SetActive(false);
+        buttonTwo.gameObject.SetActive(false);
+        instText.gameObject.SetActive(false);
+        buttonThree.gameObject.SetActive(false);
+    }
+
+}
