1bf4cb7b4e3ab6ab8897a09d86195548d7be9997
inparia <inparia@live.ca>
2020-10-08 16:50:15 -0400

    Lab 3 Object Pooling
    Lab 3 Object Pooling

---
+++ GAME2014-Lab2/Assets/ObjectPool.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ObjectPool : MonoBehaviour
+{
+    public Queue<GameObject> m_bulletPool;
+    public GameObject bullet;
+    public int MaxBullets;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _buildBulletPool();
+    }
+
+    public void _buildBulletPool()
+    {
+        m_bulletPool = new Queue<GameObject>();
+
+        for(int count = 0; count < MaxBullets; count ++)
+        {
+            var tempBullet = Instantiate(bullet);
+            tempBullet.SetActive(false);
+            m_bulletPool.Enqueue(tempBullet);
+        }    
+
+    }
+
+    public GameObject GetBullet(Vector3 position)
+    {
+        var newBullet = m_bulletPool.Dequeue();
+        newBullet.SetActive(true);
+        newBullet.transform.position = position;
+        return newBullet;
+    }
+
+    public void ReturnBullet(GameObject returnBullet)
+    {
+        returnBullet.SetActive(false);
+        m_bulletPool.Enqueue(returnBullet);
+    }
+}
